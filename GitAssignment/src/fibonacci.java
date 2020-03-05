cecfe0ff2e70395810792356e034b072b8592be8
Saeed Muhammed <saidmsaid2011@gmail.com>
2020-03-05 09:00:03 -0800

    20170114 Optimized Fibo

--- GitAssignment/src/fibonacci.java
+++ GitAssignment/src/fibonacci.java
@@ -9,19 +9,40 @@
  * @author Saeed
  */
 public class fibonacci  implements ISubscriber {
+    
+    
+     int MAX = 100; 
+   int NIL = -1; 
+  
+   int lookup[] = new int[MAX]; 
+  void initialize() 
+  { 
+    for (int i = 0; i < MAX; i++) 
+        lookup[i] = NIL; 
+  } 
+        
+   int fib(int n) 
+  { 
+    if (lookup[n] == NIL) 
+    { 
+      if (n <= 1) 
+          lookup[n] = n; 
+      else
+          lookup[n] = fib(n-1) + fib(n-2); 
+    } 
+    return lookup[n]; 
+  } 
+
+    
     @Override
 	public void notifySubscriber(String input) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, I am Fibonacci and I am notified with " + input);
+                initialize();
                 System.out.println("The Result of Fibonacci is " + fib(Integer.parseInt(input)));
 	}
-    static int fib(int n) 
-    { 
-    if (n <= 1) 
-       return n; 
-    return fib(n-1) + fib(n-2); 
-        } 
-
+        
+     
   
     
     
